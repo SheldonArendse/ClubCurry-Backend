297cc563be85d6e5fe99ab0382c3ad5cea5df48a
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-05-17 13:16:58 +0200

    Ingredient factory

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -8,4 +8,7 @@ public class Helper {
     public static boolean isNullOrEmpty(String obj){
         return obj == null || obj.isEmpty();
     }
+    public static boolean isNull(int obj){
+        return obj<=0;
+    }
 }
