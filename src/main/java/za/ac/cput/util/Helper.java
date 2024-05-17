aa544c82b3bad660be44ac6cc13dbe857635ff8b
Mohamed Zouheir <126957679+mohamedzouheir@users.noreply.github.com>
2024-05-17 12:21:58 +0200

    Simplified Helper class

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,10 +1,7 @@
 package za.ac.cput.util;
 
 public class Helper {
-    public static boolean isNullOrEmpty(String str) {
-        return str == null || str.isEmpty();
-    }
-    public static boolean isNullOrEmpty(Integer integer) {
-        return integer == null || integer.toString().isEmpty();
+    public static boolean isNullOrEmpty(Object object) {
+        return object == null || object.toString().isEmpty();
     }
 }
