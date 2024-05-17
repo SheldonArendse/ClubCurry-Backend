0389231fcce5dbb7fa5f193c59b45c7973e2316f
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-05-17 19:36:35 +0200

    Fixed driver

--- src/main/java/za/ac/cput/domain/Driver.java
+++ src/main/java/za/ac/cput/domain/Driver.java
@@ -5,8 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-
-public class Driver extends Employee{
+public class Driver{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -77,62 +76,6 @@ public class Driver extends Employee{
         }
         public Driver build(){
             return new Driver(this);
-        }
-    }
-}
-
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(registrationNumber, model, color, make);
-    }
-
-    @Override
-    public String toString() {
-        return "Vehicle{" +
-                "registration='" + registrationNumber + '\'' +
-                ", model='" + model + '\'' +
-                ", color='" + color + '\'' +
-                ", make='" + make + '\'' +
-                '}';
-    }
-
-    public static class Builder{
-        String registrationNumber;
-        String model;
-        String color;
-        String make;
-
-        public Builder setRegistration(String registrationNumber) {
-            this.registrationNumber = registrationNumber;
-            return this;
-        }
-
-        public Builder setModel(String model) {
-            this.model = model;
-            return this;
-        }
-
-        public Builder setColor(String color) {
-            this.color = color;
-            return this;
-        }
-
-        public Builder setMake(String make) {
-            this.make = make;
-            return this;
-        }
-
-        public Builder copy(Vehicle obj){
-            this.registrationNumber = obj.registrationNumber;
-            this.model = obj.model;
-            this.color = obj.color;
-            this.make = obj.make;
-            return this;
-        }
-
-        Vehicle build(){
-            return new Vehicle(this);
         }
     }
 }
