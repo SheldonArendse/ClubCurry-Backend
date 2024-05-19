1198c0771331a8afd53793011e22b8eb223e8260
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-05-19 09:16:06 +0200

    Update ITimeAllocationService.java

--- src/main/java/za/ac/cput/service/ITimeAllocationService.java
+++ src/main/java/za/ac/cput/service/ITimeAllocationService.java
@@ -2,8 +2,6 @@ package za.ac.cput.service;
 
 import za.ac.cput.domain.TimeAllocation;
 
-import java.util.List;
-
-public interface ITimeAllocationService extends ImplService<TimeAllocation, String> {
+public interface ITimeAllocationService extends IService<TimeAllocation, String> {
 
 }
