6096fb1f442e35e64490995168d21f3a5174f709
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-05-19 09:15:49 +0200

    Update IService.java

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -2,7 +2,7 @@ package za.ac.cput.service;
 
 import java.util.List;
 
-public interface ImplService <T, ID>{
+public interface IService<T, ID>{
     public T create(T obj);
     public T read(ID id);
     public T update(T obj);
