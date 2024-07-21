43b2344ea94d914c8e66c9c0a75c802eb76e25a8
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-07-21 11:46:16 +0200

    forgot this

--- src/main/java/za/ac/cput/service/GenStaffService.java
+++ src/main/java/za/ac/cput/service/GenStaffService.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.domain.GeneralStaff;
 import za.ac.cput.repository.GenStaffRepo;
-import za.ac.cput.service.interfaces.IChefService;
+import za.ac.cput.service.interfaces.IGenStaffService;
 
 import java.util.List;
 
 @Service
-public class GenStaffService implements IChefService {
+public class GenStaffService implements IGenStaffService {
 
     private GenStaffRepo genStaffRepo;
 
--- src/main/java/za/ac/cput/service/interfaces/IGenStaffService.java
+++ src/main/java/za/ac/cput/service/interfaces/IGenStaffService.java
@@ -2,5 +2,5 @@ package za.ac.cput.service.interfaces;
 
 import za.ac.cput.domain.GeneralStaff;
 
-public interface IChefService extends IService<GeneralStaff, String> {
+public interface IGenStaffService extends IService<GeneralStaff, String> {
 }
