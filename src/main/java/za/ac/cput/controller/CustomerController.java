ded11cf33b077c31683c4ad06cf07510d3f02dfe
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-07-28 15:58:48 +0200

    Update CustomerController.java

--- src/main/java/za/ac/cput/controller/CustomerController.java
+++ src/main/java/za/ac/cput/controller/CustomerController.java
@@ -27,7 +27,12 @@ public class CustomerController {
     public ResponseEntity<Customer> save(@RequestBody Customer obj) {
         // builds a customer with no addresses
         Customer buildObj = CustomerFactory.buildCustomer(obj.getEmail(), obj.getName(), obj.getSurname(), obj.getMobileNo(), obj.getPassword());
-        if(buildObj == null){
+        Customer exists = customerService.read(obj.getEmail());
+        if (buildObj == null) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
+        }
+        if (exists != null) {
+            System.out.println(exists);
             return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(customerService.save(buildObj));
@@ -43,10 +48,20 @@ public class CustomerController {
     public ResponseEntity<Customer> update(@RequestBody Customer obj) {
         //  updates with addresses
         Customer buildObj = CustomerFactory.buildCustomer(obj.getEmail(), obj.getName(), obj.getSurname(), obj.getMobileNo(), obj.getPassword());
-        if(buildObj == null){
+        Customer exists = customerService.read(obj.getEmail());
+
+        if (buildObj == null) {
+            System.out.println(1);
             return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
         }
-        return ResponseEntity.status(HttpStatus.OK).body(customerService.update(buildObj));
+
+        if (exists != null) {
+            Customer updated = customerService.update(buildObj);
+            System.out.println(updated.getAddresses());
+            return ResponseEntity.status(HttpStatus.OK).body(customerService.read(updated.getEmail()));
+        }
+        System.out.println(exists);
+        return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
     }
 
 
@@ -57,15 +72,15 @@ public class CustomerController {
 
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<Customer>>getAll() {
+    public ResponseEntity<List<Customer>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(customerService.getAll());
     }
 
     @GetMapping("/login")
-    public ResponseEntity<Customer> login(@RequestBody Customer obj){
+    public ResponseEntity<Customer> login(@RequestBody Customer obj) {
         Customer login = CustomerFactory.buildCustomer(obj.getEmail(), obj.getPassword());
 
-        if(login == null){
+        if (login == null) {
             return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
         }
 
