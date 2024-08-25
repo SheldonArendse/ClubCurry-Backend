2acc0608d8d9bcbb75d8356f091d23d4d60b25b8
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 15:54:21 +0200

    Create IngredientService.java

---
+++ src/main/java/za/ac/cput/service/IngredientService.java
@@ -0,0 +1,13 @@
+package za.ac.cput.service;
+
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Ingredient;
+import za.ac.cput.repository.IngredientRepo;
+import za.ac.cput.service.interfaces.IIngredientService;
+import za.ac.cput.service.interfaces.IService;
+
+import java.util.List;
+
+@Service
+public class IngredientService implements IIngredientService{
+}
