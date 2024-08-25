1059981060941958d9bb5a3e673b74bfd0ab52be
SheldonArendse <sheldonarendse0101@gmail.com>
2024-08-25 15:09:06 +0200

    Completed Booking Controller

--- src/main/java/za/ac/cput/controller/BookingController.java
+++ src/main/java/za/ac/cput/controller/BookingController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/booking")
 public class BookingController {
+
     private BookingService bookingService;
     private BookingGeneralStaff bookingGeneralStaff;
 
@@ -24,6 +25,7 @@ public class BookingController {
         this.bookingService = bookingService;
         this.bookingGeneralStaff = bookingGeneralStaff;
     }
+
     @PostMapping("/save")
     public ResponseEntity<Booking> save(@RequestBody Booking obj){
         Booking booking = BookingFactory.buildBooking(Time.valueOf(obj.getTime()).toLocalTime(), Date.valueOf(obj.getDate()).toLocalDate(),obj.getTableNo(),obj.getSectionNo(),obj.getStatus());
@@ -33,4 +35,29 @@ public class BookingController {
         return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
     }
 
+    @GetMapping("/read/{id}")
+    public ResponseEntity<Booking> read(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(bookingService.read(id));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Booking> update(@RequestBody Booking obj){
+        Booking booking = BookingFactory.buildBooking(obj.getBookingId(),Time.valueOf(obj.getTime()).toLocalTime(), Date.valueOf(obj.getDate()).toLocalDate(),obj.getTableNo(),obj.getSectionNo(),obj.getStatus(),obj.getBookedBy());
+        if(booking !=null){
+            if(bookingGeneralStaff.canSave(booking) != null){
+                return ResponseEntity.status(HttpStatus.OK).body(bookingService.update(booking));
+            }
+        }
+        return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(bookingService.delete(id));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Booking>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(bookingService.getAll());
+    }
 }
