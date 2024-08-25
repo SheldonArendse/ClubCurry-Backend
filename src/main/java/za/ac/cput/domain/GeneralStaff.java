c7d5cee5a99a705cce061712d285f50d5662dfd8
SheldonArendse <sheldonarendse0101@gmail.com>
2024-08-25 14:23:38 +0200

    General Staff Model Completed

--- src/main/java/za/ac/cput/domain/GeneralStaff.java
+++ src/main/java/za/ac/cput/domain/GeneralStaff.java
@@ -28,3 +28,48 @@ public class GeneralStaff extends Employee{
                 ", password='" + password + '\'' +
                 '}';
     }
+
+    public static class Builder{
+        private String id;
+
+        private String name, surname, username, password;
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder copy(GeneralStaff obj){
+            this.id = obj.id;
+            this.name = obj.name;
+            this.surname = obj.surname;
+            this.password = obj.password;
+            this.username = obj.username;
+            return this;
+        }
+
+        public GeneralStaff build(){
+            return new GeneralStaff(this);
+        }
+    }
+}
