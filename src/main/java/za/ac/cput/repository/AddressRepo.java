63a3c6dd24ad189f745009bfdc1a1e0945a6d82a
Aaniquah Dicks <222641495@mycput.ac.za>
2024-08-25 16:30:22 +0200

    JpaRepository to manage Addresses

--- src/main/java/za/ac/cput/repository/AddressRepo.java
+++ src/main/java/za/ac/cput/repository/AddressRepo.java
@@ -1,4 +1,12 @@
 package za.ac.cput.repository;
 
-public interface AddressRepo {
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Address;
+
+@Repository
+public interface AddressRepo extends JpaRepository<Address, Long> {
+
 }
