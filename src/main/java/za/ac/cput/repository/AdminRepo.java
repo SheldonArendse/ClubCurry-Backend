2c53ae5e3176a1e15ec9f94d2d67aaaf62cf8d7c
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 15:12:52 +0200

    Update AdminRepo.java
    @repo annotations added

--- src/main/java/za/ac/cput/repository/AdminRepo.java
+++ src/main/java/za/ac/cput/repository/AdminRepo.java
@@ -1,4 +1,9 @@
 package za.ac.cput.repository;
 
-public interface AdminRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Admin;
+
+@Repository
+public interface AdminRepo extends JpaRepository<Admin, String> {
 }
