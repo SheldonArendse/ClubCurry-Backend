0ee5311f9ba3e58ddd207c070ffef32036f584d4
Aaniquah Dicks <222641495@mycput.ac.za>
2024-08-25 17:02:29 +0200

    Add CORS

--- src/main/java/za/ac/cput/controller/AddressController.java
+++ src/main/java/za/ac/cput/controller/AddressController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/address")
+@CrossOrigin(origins = "*", allowedHeaders = "*") // Allow all origins and headers
 public class AddressController {
 
     private AddressService addressService;
