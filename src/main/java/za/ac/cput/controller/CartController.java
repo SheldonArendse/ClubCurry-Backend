7e21c645f47ea8e94ba7a6c1a788e597577847eb
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 18:45:57 +0200

    Update CartController.java
    save added

--- src/main/java/za/ac/cput/controller/CartController.java
+++ src/main/java/za/ac/cput/controller/CartController.java
@@ -1,4 +1,42 @@
 package za.ac.cput.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Cart;
+import za.ac.cput.facade.AddItemToCart;
+import za.ac.cput.facade.CustomerCart;
+import za.ac.cput.factory.CartFactory;
+import za.ac.cput.service.CartService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    private CartService cartService;
+    private CustomerCart customerCart;
+    private AddItemToCart addItemToCart;
+
+    @Autowired
+    public CartController(CartService cartService, CustomerCart customerCart,AddItemToCart addItemToCart) {
+        this.cartService = cartService;
+        this.customerCart = customerCart;
+        this.addItemToCart = addItemToCart;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Cart> save(@RequestBody Cart obj){
+        Cart cart = CartFactory.buildCart(obj.getCustomer());
+        if(customerCart.canSave(cart) == null){
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
+        }
+        Cart reObj = cartService.save(cart);
+        if(reObj== null){
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(reObj);
+    }
 }
