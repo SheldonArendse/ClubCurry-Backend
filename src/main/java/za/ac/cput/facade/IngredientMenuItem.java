370c071b40b5a30c2bcd388fe6cb83fcd75eac63
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 17:58:43 +0200

    Update IngredientMenuItem.java

--- src/main/java/za/ac/cput/facade/IngredientMenuItem.java
+++ src/main/java/za/ac/cput/facade/IngredientMenuItem.java
@@ -1,4 +1,17 @@
 package za.ac.cput.facade;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.ac.cput.domain.Ingredient;
+import za.ac.cput.domain.MenuItem;
+import za.ac.cput.factory.IngredientFactory;
+import za.ac.cput.service.IngredientService;
+import za.ac.cput.service.MenuItemService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
 public class IngredientMenuItem {
 }
