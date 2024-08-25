e5cc7dc32da27534673131002b229605a8847154
Aaniquah Dicks <222641495@mycput.ac.za>
2024-08-25 16:09:36 +0200

    Address Builder

--- src/main/java/za/ac/cput/domain/Address.java
+++ src/main/java/za/ac/cput/domain/Address.java
@@ -56,5 +56,74 @@ public class Address {
         return suburb;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return id == address.id && Objects.equals(streetName, address.streetName) && Objects.equals(streetNo, address.streetNo) && Objects.equals(suburb, address.suburb) && Objects.equals(customerId, address.customerId);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, streetName, streetNo, suburb, customerId);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", streetName='" + streetName + '\'' +
+                ", streetNo='" + streetNo + '\'' +
+                ", suburb=" + suburb + '\'' +
+                ", customer=" + customerId +
+                '}';
+    }
+
+    public static class Builder{
+        private long id;
+
+        private String streetName, streetNo;
+        private Suburb suburb;
+
+        private Customer customerId;
+
+        public Builder setCustomerId(Customer customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public Builder setId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setStreetName(String streetName) {
+            this.streetName = streetName;
+            return this;
+        }
+
+        public Builder setStreetNo(String streetNo) {
+            this.streetNo = streetNo;
+            return this;
+        }
+
+        public Builder setSuburb(Suburb suburb) {
+            this.suburb = suburb;
+            return this;
+        }
+
+        public Builder copy(Address obj){
+            this.id = obj.id;
+            this.streetName = obj.streetName;
+            this.streetNo = obj.streetNo;
+            this.suburb = obj.suburb;
+            this.customerId = obj.customerId;
+            return this;
+        }
+
+        public Address build(){
+            return new Address(this);
+        }
+    }
 }
