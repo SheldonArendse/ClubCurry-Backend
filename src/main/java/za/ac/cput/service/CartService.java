58ee9e461e8400ff29fb478d9876d480adb88c71
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 15:47:52 +0200

    Update CartService.java

--- src/main/java/za/ac/cput/service/CartService.java
+++ src/main/java/za/ac/cput/service/CartService.java
@@ -1,4 +1,17 @@
 package za.ac.cput.service;
 
-public class CartService {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.Cart;
+import za.ac.cput.repository.AddressRepo;
+import za.ac.cput.repository.CartRepo;
+import za.ac.cput.service.interfaces.ICartService;
+import za.ac.cput.service.interfaces.IService;
+
+import java.util.List;
+
+@Service
+public class CartService implements ICartService {
 }
