8cde8bce06697bb0a06ed9e10fd5a859016b17bd
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 15:50:08 +0200

    Create CartMenuItemsService.java
    created

---
+++ src/main/java/za/ac/cput/service/CartMenuItemsService.java
@@ -0,0 +1,14 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.CartMenuItems;
+import za.ac.cput.repository.CartMenuItemsRepo;
+import za.ac.cput.service.interfaces.ICartMenuItemsService;
+
+import java.util.List;
+
+@Service
+public class CartMenuItemsService implements ICartMenuItemsService{
+}
