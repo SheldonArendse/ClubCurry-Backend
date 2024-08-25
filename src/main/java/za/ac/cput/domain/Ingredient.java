5a2fd8f059188c9265c6e0605a5e3134f42ebbc2
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 14:34:14 +0200

    Update Ingredient.java

--- src/main/java/za/ac/cput/domain/Ingredient.java
+++ src/main/java/za/ac/cput/domain/Ingredient.java
@@ -22,4 +22,33 @@ public class Ingredient {
         this.id = obj.id;
         this.name = obj.name;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
