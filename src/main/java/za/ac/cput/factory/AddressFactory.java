7c12ee7dc4567c1da8af742a0d752d7a92bdf360
Aaniquah Dicks <222641495@mycput.ac.za>
2024-08-25 16:21:26 +0200

    Add buildAddress methods with customer validation

--- src/main/java/za/ac/cput/factory/AddressFactory.java
+++ src/main/java/za/ac/cput/factory/AddressFactory.java
@@ -1,4 +1,30 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.Customer;
+import za.ac.cput.domain.embedded.Suburb;
+import za.ac.cput.utils.Validation;
+
 public class AddressFactory {
+
+    public static Address buildAddress(String streetName, String streetNo, String suburbName, int postalCode, Customer customerId) {
+        if (Validation.isValidString(streetName) && Validation.isValidEmail(customerId.getEmail())
+                && Validation.isValidStreetNo(streetNo)
+                && Validation.isValidString(suburbName)
+                && Validation.isValidPostalCode(postalCode)) {
+
+            Suburb suburb = new Suburb.Builder()
+                    .setSuburbName(suburbName)
+                    .setPostalCode(postalCode)
+                    .build();
+
+            return new Address.Builder()
+                    .setStreetName(streetName)
+                    .setStreetNo(streetNo)
+                    .setSuburb(suburb)
+                    .setCustomerId(customerId)
+                    .build();
+        }
+        return null;
+    }
 }
