79d5a6c18f01e6ebf92fe19b4a1f1dc9475a0d55
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 16:03:06 +0200

    Update IngredientController.java

--- src/main/java/za/ac/cput/controller/IngredientController.java
+++ src/main/java/za/ac/cput/controller/IngredientController.java
@@ -1,4 +1,19 @@
 package za.ac.cput.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Ingredient;
+import za.ac.cput.domain.MenuItem;
+import za.ac.cput.facade.IngredientMenuItem;
+import za.ac.cput.factory.IngredientFactory;
+import za.ac.cput.service.IngredientService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ingredient")
 public class IngredientController {
 }
