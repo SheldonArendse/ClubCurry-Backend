377b010f137d348fb2ba9200a061de52e0384e44
Aaniquah Dicks <222641495@mycput.ac.za>
2024-08-25 16:22:44 +0200

    Add buildAddress method without customer validation

--- src/main/java/za/ac/cput/factory/AddressFactory.java
+++ src/main/java/za/ac/cput/factory/AddressFactory.java
@@ -7,11 +7,11 @@ import za.ac.cput.utils.Validation;
 
 public class AddressFactory {
 
-    public static Address buildAddress(String streetName, String streetNo, String suburbName, int postalCode, Customer customerId) {
-        if (Validation.isValidString(streetName) && Validation.isValidEmail(customerId.getEmail())
+    public static Address buildAddress(String streetName, String streetNo, String suburbName, int postalCode, Customer customerId){
+        if(Validation.isValidString(streetName) && Validation.isValidEmail(customerId.getEmail())
                 && Validation.isValidStreetNo(streetNo)
                 && Validation.isValidString(suburbName)
-                && Validation.isValidPostalCode(postalCode)) {
+                && Validation.isValidPostalCode(postalCode)){
 
             Suburb suburb = new Suburb.Builder()
                     .setSuburbName(suburbName)
@@ -22,6 +22,35 @@ public class AddressFactory {
                     .setStreetName(streetName)
                     .setStreetNo(streetNo)
                     .setSuburb(suburb)
+                    .setCustomerId(customerId)
+                    .build();
+        }
+        return null;
+    }
+    public static Address buildAddress(Long id, String streetName, String streetNo, String suburbName, int postalCode){
+        if(Validation.isValidString(streetName)
+                && Validation.isValidStreetNo(streetNo)
+                && Validation.isValidString(suburbName)
+                && Validation.isValidPostalCode(postalCode)){
+
+            Suburb suburb = new Suburb.Builder()
+                    .setSuburbName(suburbName)
+                    .setPostalCode(postalCode)
+                    .build();
+
+            return new Address.Builder()
+                    .setId(id)
+                    .setStreetName(streetName)
+                    .setStreetNo(streetNo)
+                    .setSuburb(suburb)
+                    .build();
+        }
+        return null;
+    }
+    public static Address buildAddress(Long id,  Customer customerId){
+        if(Validation.isValidEmail(customerId.getEmail()) && Validation.isValidEmail(customerId.getEmail())){
+            return new Address.Builder()
+                    .setId(id)
                     .setCustomerId(customerId)
                     .build();
         }
