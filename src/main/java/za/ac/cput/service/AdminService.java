e66c1c4843371e1a4689ae53fd2e7581a9cecd1b
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 15:45:19 +0200

    Update AdminService.java
    service repo, read and save

--- src/main/java/za/ac/cput/service/AdminService.java
+++ src/main/java/za/ac/cput/service/AdminService.java
@@ -9,5 +9,23 @@ import za.ac.cput.service.interfaces.IAdminService;
 import java.util.List;
 
 @Service
-public class AdminService {
+public class AdminService implements IAdminService{
+    private AdminRepo adminRepo;
+
+    @Autowired
+    public AdminService(AdminRepo adminRepo) {
+        this.adminRepo = adminRepo;
+    }
+
+    @Override
+    public Admin save(Admin obj) {
+        return adminRepo.save(obj);
+    }
+
+    @Override
+    public Admin read(String s) {
+        return adminRepo.findById(s).orElse(null);
+    }
+
+  
 }
