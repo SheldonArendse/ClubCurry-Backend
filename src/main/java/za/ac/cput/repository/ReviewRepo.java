38fb40eb076f953f3f9e48e12d7d1d79a171f91d
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 14:57:33 +0200

    Update ReviewRepo.java

--- src/main/java/za/ac/cput/repository/ReviewRepo.java
+++ src/main/java/za/ac/cput/repository/ReviewRepo.java
@@ -1,4 +1,9 @@
 package za.ac.cput.repository;
 
-public interface ReviewRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Review;
+
+@Repository
+public interface ReviewRepo extends JpaRepository<Review, Long>{
 }
