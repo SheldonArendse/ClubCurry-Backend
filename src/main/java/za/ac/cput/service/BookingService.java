8158c6a61439901bf85e5eac1447f653a02c612f
SheldonArendse <sheldonarendse0101@gmail.com>
2024-08-25 14:55:31 +0200

    Booking Service Interface

---
+++ src/main/java/za/ac/cput/service/BookingService.java
@@ -0,0 +1,12 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Booking;
+import za.ac.cput.repository.BookingRepo;
+import za.ac.cput.service.interfaces.IBookingService;
+
+import java.util.List;
+
+public class BookingService implements IBooking{
+}
