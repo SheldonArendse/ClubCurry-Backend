203ad17afb2740320cc80c78969f9a1a5c60430d
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 18:46:38 +0200

    Update CartController.java
    read, update delete and getall

--- src/main/java/za/ac/cput/controller/CartController.java
+++ src/main/java/za/ac/cput/controller/CartController.java
@@ -39,4 +39,32 @@ public class CartController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(reObj);
     }
+
+    @GetMapping("read/{id}")
+    public ResponseEntity<Cart> read(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.read(id));
+    }
+
+    @PutMapping("/updateItems")
+    public ResponseEntity<Cart> updateItems(@RequestBody Cart obj){
+        Cart cart = cartService.read(obj.getId());
+        if(cart == null){
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
+        }
+        if(addItemToCart.validItems(obj) == null){
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
+        }
+        Cart cartUpdate = new Cart.Builder().copy(cart).setItems(obj.getItems()).build();
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.update(cartUpdate));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.delete(id));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Cart>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.getAll());
+    }
 }
