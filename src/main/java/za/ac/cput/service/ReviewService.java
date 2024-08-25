bd77d60921919366e8b5bc54bede6dbfe5e6e669
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 15:40:17 +0200

    Update ReviewService.java

--- src/main/java/za/ac/cput/service/ReviewService.java
+++ src/main/java/za/ac/cput/service/ReviewService.java
@@ -1,4 +1,13 @@
 package za.ac.cput.service;
 
-public class ReviewService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Review;
+import za.ac.cput.repository.ReviewRepo;
+import za.ac.cput.service.interfaces.IReviewService;
+
+import java.util.List;
+
+@Service
+public class ReviewService implements IReviewService{
 }
