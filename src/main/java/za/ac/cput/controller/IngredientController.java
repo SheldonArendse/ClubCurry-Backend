42fe188fd15681e9bfde3dd9312bac18f9fa8388
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 16:04:29 +0200

    Update IngredientController.java

--- src/main/java/za/ac/cput/controller/IngredientController.java
+++ src/main/java/za/ac/cput/controller/IngredientController.java
@@ -39,4 +39,33 @@ public class IngredientController {
     public ResponseEntity<Ingredient> read(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(ingredientService.read(id));
     }
+
+    @PutMapping("/update")
+    public ResponseEntity<Ingredient> update(@RequestBody Ingredient obj){
+        Ingredient i1 = IngredientFactory.buildIngredient(obj.getId(),obj.getName());
+        if(i1 == null){
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(ingredientService.update(i1));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable Long id){
+        List<MenuItem> menuItemList = ingredientMenuItem.itemsUsingIngredient(id);
+        Ingredient i1 = ingredientService.read(id);
+
+        if(menuItemList == null){
+            return ResponseEntity.status(HttpStatus.OK).body(ingredientService.delete(id));
+        }
+
+        for(MenuItem menuItem: menuItemList){
+            menuItem.getIngredients().remove(i1);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(ingredientService.delete(id));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Ingredient>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(ingredientService.getAll());
+    }
 }
