c038e1758b4e01f7f27934f846e5320150fe550b
Aaniquah Dicks <222641495@mycput.ac.za>
2024-08-25 19:49:02 +0200

    Added CORS

--- src/main/java/za/ac/cput/controller/DeliveryController.java
+++ src/main/java/za/ac/cput/controller/DeliveryController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/delivery")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", allowCredentials = "true")
 public class DeliveryController {
 
     private final DeliveryService deliveryService;
