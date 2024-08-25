6a31a3f8ea357ac3f0d3d0f7d0350b00d57acd59
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 15:22:50 +0200

    Update CartMenuItemsRepo.java
    @repo

--- src/main/java/za/ac/cput/repository/CartMenuItemsRepo.java
+++ src/main/java/za/ac/cput/repository/CartMenuItemsRepo.java
@@ -1,5 +1,9 @@
 package za.ac.cput.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.CartMenuItems;
 
-public interface CartMenuItemsRepo {
+@Repository
+public interface CartMenuItemsRepo extends JpaRepository<CartMenuItems,Long>{
 }
