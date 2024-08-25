c25fb902225ac2d6d9813792f8fc2d440a46b80b
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 14:22:51 +0200

    Update Review.java

--- src/main/java/za/ac/cput/domain/Review.java
+++ src/main/java/za/ac/cput/domain/Review.java
@@ -7,5 +7,25 @@ import java.util.Objects;
 @Entity
 public class Review {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Rating rating;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Customer customer;
+
+    private String note;
+
+    protected Review(){}
+
+    public Review(Builder obj) {
+        this.id = obj.id;
+        this.rating = obj.rating;
+        this.customer = obj.customer;
+        this.note = obj.note;
+    }
 
 }
