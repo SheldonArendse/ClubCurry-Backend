db0b8862e7dd7f1982eb8a50fbf2b6baf0f0a470
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 14:49:35 +0200

    Update IngredientFactory.java

--- src/main/java/za/ac/cput/factory/IngredientFactory.java
+++ src/main/java/za/ac/cput/factory/IngredientFactory.java
@@ -1,4 +1,13 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Ingredient;
+import za.ac.cput.utils.Validation;
 public class IngredientFactory {
+
+    public static Ingredient buildIngredient(String name){
+        if(Validation.isValidString(name)){
+            return new Ingredient.Builder().setName(name).build();
+        }
+        return null;
+    }
 }
