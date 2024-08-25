77969cf46443115e47750825aa5e8ddf3926fcdf
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 15:59:20 +0200

    Create DriverService.java

---
+++ src/main/java/za/ac/cput/service/DriverService.java
@@ -0,0 +1,16 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Delivery;
+import za.ac.cput.domain.Driver;
+import za.ac.cput.repository.DriverRepo;
+import za.ac.cput.service.interfaces.IDriverService;
+import za.ac.cput.service.interfaces.IService;
+
+import java.util.List;
+
+@Service
+public class DriverService implements IDriverService{
+    
+}
