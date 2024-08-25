105dbccb3e6e326bf7fca5c35a2646e7221d8328
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 15:17:33 +0200

    Update CartRepo.java
    @repository

--- src/main/java/za/ac/cput/repository/CartRepo.java
+++ src/main/java/za/ac/cput/repository/CartRepo.java
@@ -1,4 +1,9 @@
 package za.ac.cput.repository;
 
-public interface CartRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Cart;
+
+@Repository
+public interface CartRepo extends JpaRepository<Cart, Long>{
 }
