a9c342ceb912a050dbfb40c71630e6a733cb7e53
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-08-25 14:40:01 +0200

    Update Driver.java

--- src/main/java/za/ac/cput/domain/Driver.java
+++ src/main/java/za/ac/cput/domain/Driver.java
@@ -6,4 +6,21 @@ import java.util.Objects;
 
 @Entity
 public class Driver extends Employee{
+
+    private double petrolAllowance;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Vehicle registration;
+
+    protected Driver(){}
+
+    public Driver(Builder obj){
+        this.petrolAllowance = obj.petrolAllowance;
+        this.registration = obj.registration;
+        this.id = obj.id;
+        this.name = obj.name;
+        this.surname = obj.surname;
+        this.password = obj.password;
+        this.username = obj.username;
+    }
 }
