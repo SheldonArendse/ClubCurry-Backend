1379838b10367102d231bd192c7de523c61f90cf
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 15:44:10 +0200

    Update AdminService.java
    imports

--- src/main/java/za/ac/cput/service/AdminService.java
+++ src/main/java/za/ac/cput/service/AdminService.java
@@ -1,4 +1,13 @@
 package za.ac.cput.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Admin;
+import za.ac.cput.repository.AdminRepo;
+import za.ac.cput.service.interfaces.IAdminService;
+
+import java.util.List;
+
+@Service
 public class AdminService {
 }
