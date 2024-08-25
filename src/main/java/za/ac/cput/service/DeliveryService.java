bb922e95b801f856c3bc1cc9b862fbcd1bdd0249
Aaniquah Dicks <222641495@mycput.ac.za>
2024-08-25 19:39:15 +0200

    CRUD methods

--- src/main/java/za/ac/cput/service/DeliveryService.java
+++ src/main/java/za/ac/cput/service/DeliveryService.java
@@ -2,44 +2,45 @@ package za.ac.cput.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Cart;
 import za.ac.cput.domain.Delivery;
 import za.ac.cput.repository.DeliveryRepo;
 import za.ac.cput.service.interfaces.IDeliveryService;
+import za.ac.cput.service.interfaces.IService;
 
 import java.util.List;
 
 @Service
 public class DeliveryService implements IDeliveryService {
 
-    private final DeliveryRepo deliveryRepo;
+    private DeliveryRepo deliveryRepo;
 
     @Autowired
     public DeliveryService(DeliveryRepo deliveryRepo) {
         this.deliveryRepo = deliveryRepo;
     }
-
     @Override
     public Delivery save(Delivery obj) {
         return deliveryRepo.save(obj);
     }
 
     @Override
-    public Delivery read(Long id) {
-        return deliveryRepo.findById(id).orElse(null);
+    public Delivery read(Long s) {
+        return deliveryRepo.findById(s).orElse(null);
     }
 
     @Override
     public Delivery update(Delivery obj) {
-        if (deliveryRepo.existsById(obj.getId())) {
+        if(deliveryRepo.existsById(obj.getId())){
             return deliveryRepo.save(obj);
         }
         return null;
     }
 
     @Override
-    public Boolean delete(Long id) {
-        if (deliveryRepo.existsById(id)) {
-            deliveryRepo.deleteById(id);
+    public Boolean delete(Long s) {
+        if(deliveryRepo.existsById(s)){
+            deliveryRepo.deleteById(s);
             return true;
         }
         return false;
