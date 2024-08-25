d43c92ce18da9720e102fb87a5001ecebce3d3e5
AaishahVW <aaishavanwitt@gmail.com>
2024-08-25 13:20:41 +0200

    Update CartMenuItems.java
    Added imports and Entity annotation

--- src/main/java/za/ac/cput/domain/CartMenuItems.java
+++ src/main/java/za/ac/cput/domain/CartMenuItems.java
@@ -1,4 +1,12 @@
 package za.ac.cput.domain;
 
+import jakarta.persistence.*;
+import za.ac.cput.domain.Cart;
+import za.ac.cput.domain.MenuItem;
+import za.ac.cput.domain.enums.SpiceLevel;
+
+import java.util.Objects;
+
+@Entity
 public class CartMenuItems {
 }
