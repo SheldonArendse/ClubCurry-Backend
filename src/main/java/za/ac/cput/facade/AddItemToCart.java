de8600be6d133f7692701b30e3850f0055e78049
AaishahVW <aaishavanwitt@gmail.com>
2024-08-26 10:10:43 +0200

    Create AddItemToCart.java
    Create Facade

---
+++ src/main/java/za/ac/cput/facade/AddItemToCart.java
@@ -0,0 +1,17 @@
+package za.ac.cput.facade;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.ac.cput.domain.Cart;
+import za.ac.cput.domain.CartMenuItems;
+import za.ac.cput.domain.MenuItem;
+import za.ac.cput.service.CartMenuItemsService;
+import za.ac.cput.service.CartService;
+import za.ac.cput.service.MenuItemService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AddItemToCart {
+}
