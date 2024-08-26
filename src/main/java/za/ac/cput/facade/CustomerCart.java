acd2111774cbb85aaf315880f06a71d972c19838
SheldonArendse <sheldonarendse0101@gmail.com>
2024-08-26 18:46:02 +0200

    Created Customer Cart Facade

---
+++ src/main/java/za/ac/cput/facade/CustomerCart.java
@@ -0,0 +1,20 @@
+package za.ac.cput.facade;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.ac.cput.domain.Cart;
+import za.ac.cput.domain.Customer;
+import za.ac.cput.service.CartService;
+import za.ac.cput.service.CustomerService;
+
+@Component
+public class CustomerCart {
+    private CartService cartService;
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerCart(CartService cartService, CustomerService customerService) {
+        this.cartService = cartService;
+        this.customerService = customerService;
+    }
+}
