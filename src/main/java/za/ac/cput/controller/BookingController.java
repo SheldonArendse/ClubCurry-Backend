c1d90826ceef9927e5d0fc805b377c8973a2198d
Mohamed Zouheir <126957679+mohamedzouheir@users.noreply.github.com>
2024-09-02 12:45:01 +0200

    Booking fix

--- src/main/java/za/ac/cput/controller/BookingController.java
+++ src/main/java/za/ac/cput/controller/BookingController.java
@@ -29,7 +29,7 @@ public class BookingController {
 
     @PostMapping("/save")
     public ResponseEntity<Booking> save(@RequestBody Booking obj){
-        Booking booking = BookingFactory.buildBooking(Time.valueOf(obj.getTime()).toLocalTime(), Date.valueOf(obj.getDate()).toLocalDate(),obj.getTableNo(),obj.getSectionNo(),obj.getStatus());
+        Booking booking = BookingFactory.buildBooking(obj.getTime(), obj.getDate(),obj.getTableNo(),obj.getSectionNo(),obj.getStatus(), obj.getBookedBy());
         if(booking !=null){
             return ResponseEntity.status(HttpStatus.OK).body(bookingService.save(booking));
         }
@@ -43,7 +43,7 @@ public class BookingController {
 
     @PutMapping("/update")
     public ResponseEntity<Booking> update(@RequestBody Booking obj){
-        Booking booking = BookingFactory.buildBooking(obj.getBookingId(),Time.valueOf(obj.getTime()).toLocalTime(), Date.valueOf(obj.getDate()).toLocalDate(),obj.getTableNo(),obj.getSectionNo(),obj.getStatus(),obj.getBookedBy());
+        Booking booking = BookingFactory.buildBooking(obj.getBookingId(),obj.getTime(), obj.getDate(),obj.getTableNo(),obj.getSectionNo(),obj.getStatus(),obj.getBookedBy());
         if(booking !=null){
             if(bookingGeneralStaff.canSave(booking) != null){
                 return ResponseEntity.status(HttpStatus.OK).body(bookingService.update(booking));
--- src/main/java/za/ac/cput/factory/BookingFactory.java
+++ src/main/java/za/ac/cput/factory/BookingFactory.java
@@ -12,10 +12,8 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class BookingFactory {
-    public static Booking buildBooking(Long id,LocalTime time, LocalDate date, int tableNo, Section sectionNo, Status status, GeneralStaff bookedBy){
+    public static Booking buildBooking(Long id,String time, String date, int tableNo, Section sectionNo, Status status, GeneralStaff bookedBy){
         if(id>0 &&tableNo>0 && sectionNo !=null && status !=null && bookedBy !=null){
-            if(LocalDate.now().isBefore(date) || LocalDate.now().isEqual(date)){
-                if(LocalTime.now().isBefore(time)){
                     return new Booking.Builder().setBookingId(id)
                             .setTime(String.valueOf(time))
                             .setDate(String.valueOf(date))
@@ -23,16 +21,14 @@ public class BookingFactory {
                             .setSectionNo(sectionNo)
                             .setBookedBy(bookedBy)
                             .setStatus(status).build();
-                }
-            }
+
+
         }
         return null;
     }
 
-    public static Booking buildBooking(LocalTime time, LocalDate date, int tableNo, Section sectionNo, Status status, GeneralStaff bookedBy){
+    public static Booking buildBooking(String time, String date, int tableNo, Section sectionNo, Status status, GeneralStaff bookedBy){
         if(tableNo>0 && sectionNo !=null && status !=null && bookedBy !=null){
-            if(LocalDate.now().isBefore(date) || LocalDate.now().isEqual(date)){
-                if(LocalTime.now().isBefore(time)){
                     return new Booking.Builder()
                             .setTime(String.valueOf(time))
                             .setDate(String.valueOf(date))
@@ -40,23 +36,21 @@ public class BookingFactory {
                             .setSectionNo(sectionNo)
                             .setBookedBy(bookedBy)
                             .setStatus(status).build();
-                }
-            }
+
+
         }
         return null;
     }
-    public static Booking buildBooking(LocalTime time, LocalDate date, int tableNo, Section sectionNo, Status status){
+    public static Booking buildBooking(String time, String date, int tableNo, Section sectionNo, Status status){
         if(tableNo>0 && sectionNo !=null && status !=null ){
-            if(LocalDate.now().isBefore(date) || LocalDate.now().isEqual(date)){
-                if(LocalTime.now().isBefore(time)){
                     return new Booking.Builder()
                             .setTime(String.valueOf(time))
                             .setDate(String.valueOf(date))
                             .setTableNo(tableNo)
                             .setSectionNo(sectionNo)
                             .setStatus(status).build();
-                }
-            }
+
+
         }
         return null;
     }
